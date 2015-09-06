a90c96c2a9ba414238317efc6fb7ce7f900758e7
Jon Zingale <jon@jackrabbitsystems.com>
2015-09-06 00:22:38 -0600

    a big step toward files as arrays

--- file_ary.rb
+++ file_ary.rb
@@ -4,62 +4,113 @@
 	EXAMPLE = "#{FILES_PATH}/color_distributions.csv"
 	FILE1 = "#{FILES_PATH}/file1.csv"
 	FILE2 = "#{FILES_PATH}/file2.csv"
+	TEMP = "#{FILES_PATH}/tmp.csv"
+	TEMP2 = "#{FILES_PATH}/tmp2.csv"
 
+
+	# TODO: DATA
+	# will want the first file to not already have counts.
 
 	class FileAry
 		# keep things as strings.
-		attr_accessor :read, :head, :lines, :path
+		attr_accessor :read, :head, :lines, :path, :count
 		def initialize(path)
 			@path = path
 			@read = File.open(path,'r').read
 			@head = read.lines.first
 			@lines = read.lines
+			@count = lines.count
+		end
+
+		# not sure how this works
+		# def rename(new_name_path)
+		# 	File.rename(self.path, new_name_path)
+		# end
+
+		def delete
+			File.open(self.path,'w')
+			@read = File.open(path,'r').read
+			@head = read.lines.first
+			@lines = read.lines
+			@count = lines.count
+		end
+
+		# too slow for each line in chunks?
+		def append(content)
+			File.open(path,'a'){|target| target << content}
+			new_file = FileAry.new(path)
+			@read = new_file.read
+			@head = read.lines.first
+			@lines = read.lines
+		end
+
+		def copy_from(src)
+			File.open(self.path,'w'){|file| file << src.read}
+			@read = src.read
+			@head = read.lines.first
+			@lines = read.lines
+			@count = lines.count
 		end
 
 		def same_as_first_count
 			self.lines.count{|i| self.head == i }
 		end	
 
-		def append(content)
-			File.open(path,'a'){|file| file << content}
-		end
-
-		def copy(trg)
-			File.open(trg.path,'w'){|file| file << self.read}
-		end
-
 		def delete_same_as(content)
-			self.lines.reject!{|line| line == content}
-		end
+			temp = FileAry.new(TEMP)
+			temp.delete
 
-		def rename(trgt,new_name_str)
-			File.rename(@path, new_name_str)
+			File.open(temp.path,'a') do |file|
+				File.open(self.path,'r').each_line do |line|
+					file << line if line != content
+				end
+			end
+
+			temp = FileAry.new(TEMP)
+			self.copy_from(temp)
+			@read = temp.read
+			@head = read.lines.first
+			@lines = read.lines
+			@count = lines.count
+			temp.delete
 		end
 
 	end
-	# 	# POSTPROCESSING
-	# 	def sort_csv_processor
-	# 		File.open(COLOR_FILE2, 'w')
-	# 		csv = CSV.read(COLOR_FILE)
-	# 		sort_colors_in_file(csv)
-	# 	end
-	
-	# 	def sort_colors_in_file(ary)
-	# 		until ary.empty?
-	# 			ins, ary = ary.partition{|color,count| color == ary[0][0] }
-	
-	# 			count = ins.map{|col,cnt| cnt.to_i}.inject :+
-	# 			color = ins[0][0]
-	
-	# 			CSV.open(COLOR_FILE2, 'a'){|csv| csv << [color,count] }
-	# 			sort_colors_in_file(ary)
-	# 		end
-	# 	end
 
+
+	def process_color(file_ary,temp2)
+		unless file_ary.head.nil?
+			# do this in chunks i suspect
+			head_count = file_ary.same_as_first_count
+			ary_head = file_ary.head
+
+			file_ary.delete_same_as(ary_head)
+			file_ary = FileAry.new(file_ary.path)
+
+			# TODO TODO TODO
+			# look into truncate and other such
+			# methods that might be faster than append
+			# it seems I have to use an array at least here.
+			# maybe I can write eacy to a file?
+			mt_head = /\-\d+/.match(ary_head)[0] #<--- because of stupid format
+			temp2.append("#{mt_head},#{head_count}\n")
+			process_color(file_ary,temp2)
+		end
+	end
+
+	def color_counts(file_ary) # that is self
+		temp2 = FileAry.new(TEMP2)
+		temp2.delete
+
+		process_color(file_ary,temp2)
+		temp2 = FileAry.new(TEMP2)
+	end
 
 	it = FileAry.new(FILE1)
 	that = FileAry.new(FILE2)
+	that.copy_from(it)
 
-that.delete_same_as(it.head)
+	color_counts(that)
+	
 
 	byebug ; 4
