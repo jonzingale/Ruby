ae506203dc55a6a8c36e532f5f6c4c634afc4229
Jon Zingale <jon@jackrabbitsystems.com>
2015-09-25 10:56:22 -0600

    prehashed nodes

--- trophic_level.rb
+++ trophic_level.rb
@@ -78,15 +78,17 @@ include Graphs
 		attr_reader :edges, :nodes, :id
 
 		def initialize(graph)
+			@graph = graph
 			@edges = graph.edges
 			@nodes = graph.nodes
 			@count = @nodes.count
 			@id = Matrix.identity(@count)
+			@sources = @nodes.select{|n| self.is_source?(n)}
+			@eval = Vector.elements([1] * @count) # todo: if source then 0
 		end
 
 		def has_node?(node)
 			@graph.has_node?(node)
-			# @nodes.any?{|n|n==node}
 		end
 
 		def is_source?(node)
@@ -98,7 +100,6 @@ include Graphs
 		# Note: this is directed.
 		def has_edge?(node_1, node_2)
 			@graph.has_edge?(node_1, node_2)
-			# @edges["#{node_1}_#{node_2}"].present?
 		end
 
 		def tropic_height(node)
@@ -127,45 +128,56 @@ include Graphs
 		# var(i) = sigma (k-xi)^2 * pi(k)
 		# where sigma ranges from k=0 to infinity
 		# k being a path length and xi and pi are
-		# the node and probability associated with
-		# energy reaching xi. k and xi being the
-		# same type SET or index or otherwise.
-		# then convert to integer for differencing.
+		# the trophic position and probability 
+		# associated with energy reaching xi.
+		# alternatively .. .
 
-		# again, as with path_length_probability,
-		# this may be a bitch to calculate. A second
-		# similar measure is based on the transition
-		# matrix.
 		# var(i) = sigma (xj - ri)^2 tij
 		def trophic_spec(node)
 			# let ri = xi - 1
 
+			# CHANGE FOR NODE
+			index = @nodes.index{|i| i == node}
+			ri = self.trophic_position[index] - 1
+
+			# CHANGE FOR NODE
+			ary = @nodes.map.with_index do |elem,j|
+				xj = self.trophic_position[j]
+				(xj - ri)**2 * self.transition[index,j]
+			end.inject :+
 
 		end
 
-		def trophic_position_vector
+		def trophic_position
 			# y = (I - Q)^-1 * vect 1
 			# Q is the non-source columns
 			mtx, non_source = self.transition, []
 
+			# the single arrow on the source
+			# is actually considered a non-source,
+			# but then singular matrix :(
 			mtx.column_vectors.each_with_index do |col,i|
 				non_source << (col[i] == 0 ? col : [0] * @count)
 			end
 
-			one_vect = Vector.elements([1] * @count)
+			# [0,1,2.08,1.47,2.33] x
+			# [0,0,0.95,0.99,0.85] sig
+			# [0,0,0.57,0.93,0.13] sig_h
 			q_matrix = Matrix.columns(non_source)
 			n_matrix = (@id - q_matrix).inverse
-			y_vect = n_matrix * one_vect
+			y_vect = n_matrix * @eval
 		end
 
 		def transition
 			# the probability that a unit of energy entering
 			# component m was obtained directly from component n
+
+			# CHANGE FOR NODE
 			node_pair_rows = @nodes.map{|n| @nodes.map{|m| [n,m]}}
 
 			weight_rows = node_pair_rows.map do |row|
 				row.map do |s,t|
-					self.is_source?(s) && s==t ? 1 :
+					self.is_source?(s) && s==t ? 1 : # 1
 					has_edge?(s,t) ? edges["%s_%s" % [s,t]].last : 0
 				end
 			end
@@ -230,14 +242,50 @@ sources = levine.nodes.select{|node| levine.is_source?(node) }
 
 # an absorbing markov chain.
 matrix = levine.transition
-trophic_vect = levine.trophic_position_vector
+trophic_vect = levine.trophic_position.map{|t| t.to_f.round 2}
 
 pp_it(trophic_vect)
 pp_it(matrix)
 
+var_3 = levine.trophic_spec('3')
 
 
+# Sarah's Graph
+sarah = DiGraph.new
+sarah.add_nodes(('a'..'o').map &:to_s)
+sarah.add_edge('a','b',Rational(1.0))
+sarah.add_edge('b','c',Rational(1/2.0))
+sarah.add_edge('c','d',Rational(1.0))
+sarah.add_edge('d','e',Rational(1.0))
+sarah.add_edge('e','f',Rational(1/3.0))
+sarah.add_edge('g','h',Rational(1.0))
+sarah.add_edge('h','c',Rational(1/2.0))
+sarah.add_edge('i','j',Rational(1.0))
+sarah.add_edge('j','k',Rational(1.0))
+sarah.add_edge('k','l',Rational(3/4.0))
+sarah.add_edge('l','m',Rational(1.0))
+sarah.add_edge('m','f',Rational(2/3.0))
+sarah.add_edge('n','o',Rational(1.0))
+sarah.add_edge('o','l',Rational(1/4.0))
 
+tr_sarah = Levine.new(sarah)
+
+var_f = tr_sarah.trophic_spec('f')
+
+
+# a simple third
+simple = DiGraph.new
+simple.add_nodes((0..5).map &:to_s)
+simple.add_edge('0','2',0.5)
+simple.add_edge('1','2',0.5)
+
+simple.add_edge('2','3',1)
+simple.add_edge('3','4',1)
+simple.add_edge('4','5',1)
+
+
+tr_simple = Levine.new(simple)
+var_2 = tr_simple.trophic_spec('2')
 
 
 byebug ; 4
