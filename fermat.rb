d7f4634ac8c32e03d422d64aa742087a73e25926
Jon Zingale <jon@jackrabbitsystems.com>
2015-12-12 02:32:39 -0700

    updated fermat tests

--- fermat.rb
+++ fermat.rb
@@ -1,12 +1,14 @@
-# require 'byebug'
-# require 'benchmark'
+require 'byebug'
+require 'benchmark'
 
 BEST_TIME = 4.720000.freeze
+MERSENNE = (2**4423 - 1).freeze
 PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47].freeze
 
 def lookup(num) ; PRIMES.any?{|p| p == num} ; end
 
 def fermat(num,tol=2)
+	# even?
 	num < 48 ? lookup(num) : rands(num/2).all?{|a| rmod(a,num) == a}
 end
 
@@ -25,12 +27,12 @@ def fprimats tol=2, lim=100
 	(2..lim).select{|n| fermat n}
 end
 
-puts "#{fprimats}"
-
 Benchmark.bm do |x|
 	x.report{ fprimats(2, 1000000) }
+	x.report{ fermat MERSENNE}
 end
+puts "#{fprimats 2, 10**3}"
 
-# byebug ; 4
+byebug ; 4
 
 
