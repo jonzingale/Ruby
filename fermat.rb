e73479448688891efe1256ea82bcd771a5c37783
Jon Zingale <jon@jackrabbitsystems.com>
2015-12-18 12:11:54 -0700

    fermat optimization, suggestion to thread

--- fermat.rb
+++ fermat.rb
@@ -1,31 +1,45 @@
 require 'benchmark'
 require 'byebug'
 
-BEST_TIME = 51.570000.freeze
-MERSENNE = (2**4423 - 1).freeze
+# Todo: threading.
 PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47].freeze
+MERSENNE = (2**4423 - 1).freeze
+BEST_TIME = 49.790000.freeze
 
-def lookup(num) ; PRIMES.any?{|p| p == num} ; end
+# 49.790000 seconds for fermat primes < 10 M
+def primates(tol=2, lim=100) ; (2..lim).select{|n| fermat n} ; end
+def rmod(base, pow) ; rmod_worker base, pow, pow ; end
 
 def fermat(num,tol=2)
 	num != 2 && num.even? ? false :
 	num < 48 ? lookup(num) : rands(num/2).all? {|a| rmod(a,num) == a }
 end
 
-def rmod base, pow, row=nil	
-	(row = row||pow) < 4 ? (base**row) % pow :
-	row.even? ? (rmod(base, pow, row/2)**2) % pow :
-	(base * rmod(base, pow, row/2)**2) % pow
+private
+
+def lookup(num) ; PRIMES.any?{|p| p == num} ; end
+
+def rmod_worker base, pow, row
+	row < 4 ? (base**row) % pow :
+	row.even? ? (rmod_worker(base, pow, row/2)**2) % pow :
+	(base * rmod_worker(base, pow, row/2)**2) % pow
 end
 
+#######
 def rands num, tol=2 # psuedo-shuffle
 	r = {} ; (r[rand(num)] = true) while (r.length < tol) ; r.keys
 end
 
-# 51.570000 seconds for fermat primes < 10 M
-def primates tol=2, lim=100
-	(2..lim).select{|n| fermat n}
+# may be better than rands for large arrays
+# seek optimization in this direction.
+def b_day_shuffle len
+	lim = (Math.log(4.8265592) * len).floor
+	(0...len).map do
+		k = (lim-lim**2)/(2*Math.log(0.5))
+		rand(k) % lim
+	end
 end
+########
 
 def test 
 	Benchmark.bm do |x|
