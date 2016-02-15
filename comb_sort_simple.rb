654a10a2e150b71951f6fff92b387f0a34ff2755
Jonathan Zingale <jonzingale@gmail.com>
2016-02-14 19:11:38 -0700

    much cleaner comb_sort

--- comb_sort_simple.rb
+++ comb_sort_simple.rb
@@ -1,15 +1,14 @@
-def swap(ary, j, k)
-	ary[k], ary[j] = ary[j], ary[k]
-end
-
+require 'byebug'
 def comb ary
-	[*1..ary.count].each do |k|
-		[*1..ary.count-k].each{|j| swap(ary, j, k) if ary[k] < ary[j]}
+	c = ary.count
+	pairs = [*1...c-1].inject([]) do |pairs, k|
+		pairs += [*0...k].map{|j| [c-k, j]}
 	end
+
+	pairs.each do |k, j| ; k += j
+		ary[k], ary[j] = ary[j], ary[k] if ary[k] < ary[j]
+	end ; ary
 end
 
 ary = [10, 5, 12, 2, 4, 3, 8, 11, 7, 1, 9, 6]
-
-puts comb ary
-
-
+puts comb(ary)
