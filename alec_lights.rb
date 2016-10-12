a752e7eead61cd181f4cb4f04e662e634a4859c7
Jonathan Zingale <jonzingale@gmail.com>
2016-10-12 16:24:27 -0600

    with zekes simplification

--- alec_lights.rb
+++ alec_lights.rb
@@ -1,45 +1,43 @@
 require 'byebug'
 
 class Stringy
-  attr_reader :str, :ary
+  attr_reader :ary
 
   def initialize
-    @top = 255
-    @ary = [1]
+    @ary = [255]
   end
 
   def put it
     puts "%08d" % it.to_s(2)
   end
 
-  def modify str, n
-    if n == 0
-      str ^ 3
-    elsif n == 7
-      str ^ 192
-    else
-      val = 7 * 2 ** (n-1)
-      str ^ val
-    end
+  def modify bits, n
+    # if n == 0
+    #   bits ^ 3
+    # elsif n == 7
+    #   bits ^ 192
+    # else
+    #   val = 7 * 2 ** (n-1)
+    #   bits ^ val
+    # end
+
+    # mask 0b111 shifted and bounded before xor
+    # bits ^ (7 << n-1) % 256
+    bits ^ (0b111 << n-1) % 0b100000000
   end
 
   def accum
-    while @ary.length < @top
-      @ary.each do |str|
+    while ary.length < 255/2
+      @ary.each do |bits|
         (0..7).each do |n|
-          val = modify str, n
-          unless @ary.include?(val)
-            @ary << val
-            puts val
-          end
+          val = modify bits, n
+          @ary << val unless @ary.include?(val)
         end
       end
-    end
+    end ; puts @ary.sort.to_s
   end
 end
 
-boundary = "%08d" % (255 ^ 3).to_s(2)
+system('clear')
 it = Stringy.new
 it.accum
-
-byebug ; 3
