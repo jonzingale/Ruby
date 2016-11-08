6416d9955fa9686eddb2f95e964d343052c8174a
Jon Zingale <jon@jackrabbitsystems.com>
2016-11-08 12:21:44 -0700

    blue_creature_control_redux, better cmc counting

--- mtg/mtg_parser.rb
+++ mtg/mtg_parser.rb
@@ -5,22 +5,27 @@ require 'json'
 include Decks
 
 UsefulMethods = [:some_hand, :cmc_totals, :lands, :creatures,
-                 :sorceries, :instants, :enchantments, :artifacts]
+                 :sorceries, :instants, :enchantments, :artifacts,
+                 :planeswalkers]
+
 file = File.read('./allcards.json')
 Json = JSON.parse(file)
 
 class Deck
-  Types = [:Land, :Artifact, :Enchantment, :Sorcery, :Instant, :Creature]
+  Types = [:Land, :Artifact, :Enchantment, :Sorcery, :Instant, :Creature,
+           :Planeswalker]
 
   ColorKeys = {'Blue' => 'U', 'Green' => 'G', 'White' => 'W',
                'Black' => 'B', 'Red' => 'R'}
 
   attr_reader :deck, :stack, :cmc_totals, :lands, :creatures, :artifacts,
-              :sorceries, :instants, :enchantments
+              :sorceries, :instants, :enchantments, :planeswalkers
+
   def initialize deck
     @deck = deck
     @stack = mk_stack
     @cmc_totals = cmc_distribution
+    @planeswalkers = get_by_type 'Planeswalker'
     @enchantments = get_by_type 'Enchantment'
     @creatures = get_by_type 'Creature'
     @artifacts = get_by_type 'Artifact'
@@ -30,7 +35,7 @@ class Deck
   end
 
   def deck_data
-    stack.inject({}) {|hash, k| hash.merge!({k => Json[k]})}
+    stack.inject({}) { |hash, k| hash.merge!({k => Json[k]}) }
   end
 
   def get_by_type type
@@ -60,13 +65,14 @@ class Deck
     total_cmc = stack.map do |card|
       cmc = Json[card]['cmc']
       cmc.nil? ? 0 : cmc
-    end.sort
+    end.sort ; distr = {}
 
-    total_cmc.inject({}) do |distr, n|
-      count = total_cmc.count { |t| t == n }
-      total_cmc.reject! { |c| c == n }
-      distr.merge!({n => count})
-    end
+    until total_cmc.empty?
+      head = total_cmc.first
+      tally = total_cmc.count {|t| t == head }
+      distr.merge!({head => tally})
+      total_cmc.reject! {|c| c == head }
+    end ; distr
   end
 end
 
@@ -74,24 +80,28 @@ def useful_data deck
   system('clear')
   UsefulMethods.each do |sym|
     datum = deck.send(sym)
-    total = datum.values.inject(:+) if datum.is_a?(Hash)
-    puts "\n" ; puts "#{sym.to_s}: #{total}"
+    totals = datum.values.inject(:+) if datum.is_a?(Hash)
+    puts "\n" ; puts "#{sym.to_s}: #{totals}"
     puts datum
   end
 end
 
 bcc = Deck.new(Decks.blue_creature_control)
+bcc2 = Deck.new(Decks.blue_creature_control_redux)
 cfd = Deck.new(Decks.corwins_fire_deck)
 cfz = Deck.new(Decks.cards_from_zeke)
+deck_1 = Deck.new(Decks.deck_1)
 
-useful_data bcc
+useful_data bcc2
 
 # byebug ; 3
 
 # :: {Name => Multiplicity}
-# deck = Decks.blue_creature_control
-# {0=>21, 1=>14, 2=>13, 3=>10, 5=>2}
-# deck = Decks.deck_1
-# {0=>21, 1=>14, 2=>17, 3=>11, 5=>4}
-# deck = Decks.corwins_fire_deck
-# {0=>20, 1=>13, 2=>15, 3=>4, 5=>4}
+# blue_creature_control_redux
+# {0=>22, 1=>12, 2=>16, 3=>8, 5=>2, 8=>1}
+# blue_creature_control
+# {0=>21, 1=>14, 2=>13, 3=>10, 4=>3, 5=>2}
+# deck_1
+# {0=>21, 1=>14, 2=>17, 3=>11, 4=>3, 5=>4}
+# corwins_fire_deck
+# {0=>20, 1=>13, 2=>15, 3=>4, 4=>4, 5=>4}
