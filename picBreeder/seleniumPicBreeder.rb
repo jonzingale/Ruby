698890e8df9c246ad3d9f6eefd9c1b34122db3c0
Jon Zingale <jonzingale@gmail.com>
2018-05-22 21:43:05 -0600

    applet fucked

--- picBreeder/seleniumPicBreeder.rb
+++ picBreeder/seleniumPicBreeder.rb
@@ -8,6 +8,7 @@ require "rest-client"
 
 
 SCRATCH_URL = "http://picbreeder.org/user/editgenome.php?sid=-1&pid=-1"
+SOAP_URL = "http://www.picbreeder.org:8080/axis/services/WebNeatClient?wsdl"
 
 class Agent
   attr_accessor :driver
@@ -25,12 +26,26 @@ class Agent
   def quit
     @driver.quit
   end
+
+  def url
+    @driver.current_url
+  end
 end
 
+# <applet code="client.gui.Applet" archive="client.jar" width="709" height="673" align="middle">
+#   <param name="seriesId" value="-1">
+#   <param name="parentId" value="-1">
+#   <param name="username" value="anonymous">
+#   <param name="password" value="anonymous">
+#   <param name="webservices" value="http://www.picbreeder.org:8080/axis/services/WebNeatClient?wsdl">
+# </applet>
 
 agent = Agent.new
 
-element = agent.driver.find_element(:name, "username")
+elem1 = agent.driver.find_element(:xpath, "//applet")
+puts agent.driver.find_element(:xpath => "//body").text
+
+# agent.driver.find_elements(:xpath, "//script")
 byebug
 
 4
