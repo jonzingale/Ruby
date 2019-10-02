7674aaecf047483b32fbc66b3f1601432ad80b14
Jon Zingale <jonzingale@gmail.com>
2019-10-02 15:56:47 -0600

    towards generalized scraping

--- DandD/yoshimo_selenium.rb
+++ DandD/yoshimo_selenium.rb
@@ -5,12 +5,27 @@ require 'json'
 Yoshimo_Login = 'https://www.dndbeyond.com/profile/Mortekai/characters/3641195'
 
 AbilityFields = %w(strength dexterity constitution intelligence wisdom charisma)
-AbilitySel = "div[%i]//div[@class='ct-ability-summary__primary']"
-AllAbilitySel = '//div[@class="ct-quick-info__abilities"]'
+AbilitySel = "//div[@class='ct-quick-info__abilities']//div[%i]//div[@class='ct-ability-summary__primary']"
 
+PassivityFields = %w(perception investigation insight)
+PassiveSel = "//div[@class='ct-senses__callout-value']"
+
+SavingThrowsFields = %w(strength dexterity constitution intelligence wisdom charisma)
+SavingThrowsSel = "//div[@class='ct-saving-throws-summary__ability-modifier']"
+
+ProficiencySel = "ct-proficiency-bonus-box__value"
 NameSel = "ct-character-tidbits__name"
 LevelSel = "ct-character-tidbits__xp-level"
 ACSel = "ct-armor-class-box__value"
+WalkingSel = 'ct-speed-box__box-value'
+MaxXP = "ct-health-summary__hp-number"
+
+# TODO:
+# BUILD AS AN API, THINK ABOUT STRUCTURING AS JSON
+# Perhaps use a Struct.new so that all stats are hashes
+# and a Json can be built up.
+# Customer = Struct.new(:name, :address) do
+
 class Agent
   attr_accessor :driver, :options, :page, :wait
 
@@ -28,42 +43,78 @@ class Agent
 
   def get_page
     driver.get(Yoshimo_Login)
-    sleep(2)
+    sleep(1)
     driver
   end
 end
 
 class Character
-  attr_accessor :name, :level, :armor_class, :abilities
-
-  # Perhaps use a Struct.new so that all stats are hashes
-  # and a Json can be built up.
+  attr_accessor :name, :level, :armor_class, :abilities, :proficiency,
+    :walking_speed, :maxXP, :passives, :saving_throws
 
   def initialize(page)
     @page = page
-    @name = page.find_element(class: NameSel).text
-    @level = page.find_element(class: LevelSel).text
-    @armor_class = page.find_element(class: ACSel).text
-    @abilities = get_abilities
+    @abilities = {}
+    @passives = {}
+    @saving_throws = {}
+
+    @name = get_text(NameSel)
+    @level = get_text(LevelSel)
+    @armor_class = get_text(ACSel)
+    @proficiency = get_text(ProficiencySel)
+    @walking_speed = get_text(WalkingSel)
+    @maxXP = get_text(MaxXP)
+
+    get_abilities
+    get_passives
+    get_saving_throws
+    # byebug
   end
 
-  def get_abilities(abHash={})
-    elem = @page.find_element(xpath: AllAbilitySel)
-
-    AbilityFields.each_with_index do |a, i|
-       abScore = elem.find_element(xpath: AbilitySel % (i+1)).text
-       abHash[a] = abScore.gsub("\n", '')
+  def get_text(selector, type='c')
+    case type
+    when 'c'
+      @page.find_element(class: selector).text.gsub("\n", '')
+    when 'x'
+      @page.find_element(xpath: selector).text.gsub("\n", '')
+    else
+      'Error Bad Selector Type for get_text'
     end
-
-    abHash
   end
+
+  def get_saving_throws # perhaps generalize and include passives and abilities
+    saving_throws = @page.find_elements(xpath: SavingThrowsSel)
+
+    saving_throws.each_with_index do |st, i|
+      @saving_throws[SavingThrowsFields[i]] = st.text.gsub("\n",'')
+    end
+  end
+
+  def get_passives # perhaps generalize and include passives and abilities
+    passives = @page.find_elements(xpath: PassiveSel)
+
+    passives.each_with_index do |st, i|
+      @passives[PassivityFields[i]] = st.text.gsub("\n",'')
+    end
+  end
+
+  def get_abilities
+    AbilityFields.each_with_index do |a, i|
+      @abilities[a] = get_text(AbilitySel % (i+1), 'x')
+    end
+  end
+
 end
 
 
 def process
   agent = Agent.new
   yoshimo = Character.new(agent.page)
-  puts [:name, :level, :armor_class, :abilities].map {|sym| yoshimo.send(sym).to_s}
+
+  values = [:name, :level, :armor_class, :proficiency,
+    :walking_speed, :maxXP, :passives, :saving_throws, :abilities]
+
+  puts values.map {|sym| yoshimo.send(sym).to_s}
   agent.quit
 end
 
