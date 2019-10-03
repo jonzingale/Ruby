73aeea232e10ec8cbb6a9780eb350d24558b3ad2
Jon Zingale <jonzingale@gmail.com>
2019-10-03 15:08:31 -0600

    agent to character_agent

--- DandD/agent.rb
+++ DandD/agent.rb
@@ -1,20 +1,22 @@
+DndStub = 'https://www.dndbeyond.com/profile/Mortekai/characters/'
+
 class Agent
   attr_accessor :driver, :options, :page, :wait
 
-  def initialize
+  def initialize(character_id)
     @options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
     @driver  = Selenium::WebDriver.for(:chrome, options: options)
     @wait = Selenium::WebDriver::Wait.new(:timeout => 15)
     @driver.manage.window.resize_to(1440, 1080)
-    @page = get_page
+    @page = get_character_page(character_id)
   end
 
   def quit
     driver.quit
   end
 
-  def get_page
-    driver.get(Yoshimo_Login)
+  def get_character_page(character_id)
+    driver.get(DndStub+character_id)
     sleep(1)
     driver
   end
