9dfa70614d6efd8cd8204d0bc7681b71a2f3ecad
Jon Zingale <jonzingale@gmail.com>
2020-03-17 17:30:10 -0600

    extened to include counties deaths and recoveries

--- covid19_nm/covid_nm_scraper.rb
+++ covid19_nm/covid_nm_scraper.rb
@@ -4,31 +4,67 @@ require 'byebug'
 require 'date'
 require 'csv'
 
+DATE = Date.today.strftime('%Y-%m-%d')
+TIME = DateTime.now.strftime('%I:%M %p')
+
 COVID_URL = 'https://e.infogram.com/6280a269-a8ec-4d0b-8810-363d5057e67e?parent_url=http%3A%2F%2Fnmindepth.com%2F2020%2F03%2F13%2Fmap-new-mexico-covid-19-cases%2F'
-NOW = Date.today.strftime('%Y-%m-%d')
 DATA_CSV = "data.csv".freeze
 
+CASE_REGEX = /currently (\d+|no) cases/i
+DEATH_REGEX = /(\d+|no) ?reported? deaths/i
+RECOVERY_REGEX = /(\d+|no) ?reported? recoveries/i
+COUNTIES_REGEX = /"data":\[\[(.+)\]\]/
+
+class Agent
+  attr_accessor :body, :counties, :total_cases, :deaths, :recoveries
+
+  def initialize
+    @body = get_body
+    @counties = get_effected_counties
+    @total_cases = get_case_by_type(CASE_REGEX)
+    @deaths = get_case_by_type(DEATH_REGEX)
+    @recoveries = get_case_by_type(RECOVERY_REGEX)
+  end
+
+  def get_body
+    agent = Mechanize.new
+    agent.redirect_ok = true
+    landing_page = agent.get(COVID_URL)
+    landing_page.body
+  end
+
+  def get_case_by_type(regex)
+    val = regex.match(@body)[1]
+    val == 'no' ? 0 : val.to_i
+  end
+
+  def present?(val)
+    !(val.nil? || val.empty?)
+  end
+
+  def get_effected_counties
+    json_match = /"data":\[\[(.+)\]\]/.match @body
+    json = JSON.parse("{#{json_match}}")['data'][0]
+    json.each_with_object({}) do |rec, hh|
+      rec[1] = 0 unless present?(rec[1])
+      hh.merge!(rec[0] => rec[1].to_i)
+    end
+  end
+end
+
+def return_covid19_results
+  CSV.read(DATA_CSV)
+end
+
 def process
-  last_date = get_last_date
-  to_csv(NOW, get_num_cases) if  last_date < NOW
-end
+  agent = Agent.new
 
-def get_num_cases
-  agent = Mechanize.new
-  agent.redirect_ok = true
-  landing_page = agent.get(COVID_URL)
-  body = landing_page.body
-  num_cases = /there are currently (\d+)/i.match(body)[1]
-  num_cases.to_i
-end
-
-def get_last_date
-  csv = CSV.read(DATA_CSV, headers: true, return_headers: false)
-  Date.parse(csv[-1][0]).strftime('%Y-%m-%d')
-end
-
-def to_csv(date, num_of_cases)
-  CSV.open(DATA_CSV, 'a') { |csv| csv << [date, num_of_cases] }
+  CSV.open(DATA_CSV, 'a') do |csv|
+    data = [DATE, TIME, agent.total_cases, agent.deaths, agent.recoveries]
+    data << agent.counties.to_s
+    csv << data
+  end
 end
 
 process
+# return_covid19_results
