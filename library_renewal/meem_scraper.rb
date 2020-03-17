9a246910a7ddaf37aabff314914270899056ab0c
Jon Zingale <jonzingale@gmail.com>
2020-03-16 19:54:22 -0600

    revamped library scraper

--- library_renewal/meem_scraper.rb
+++ library_renewal/meem_scraper.rb
@@ -18,26 +18,25 @@ SESSION_SEL = './/input[@name="session"]'.freeze
 EXPIRATION_COL_SEL = './/a[@class="normalBlackFont2"]/parent::td'.freeze
 BOOK_SEL = './/table[@class="tableBackgroundHighlight"]//table[@class="tableBackground"]/parent::td/parent::tr'.freeze
 
-DATA_HEADERS = %w(ACCOUNT EXPIRATION NEXT_DUE CHECK_TODAY ACTIVE).freeze
-KEYS = [:borrower_id, :expiration, :next_due, :check_today, :active].freeze
+HEADERS = %w(ACCOUNT EXPIRATION NEXT_DUE HAS_RUN ACTIVE).freeze
 
 RENEW_KEYS = './/input[@name="renewitemkeys"]'.freeze
 AUTHOR_REGEX = /by (.+), \d*/.freeze
 EDITOR_REGEX = /by\] (.+) \;/.freeze
 
 # a source directory off of crude. --untracked.
-FILES_PATH = File.expand_path('./../../src/meem_library', __FILE__).freeze
+FILES_PATH = File.expand_path('./../data', __FILE__).freeze
 MEEM_INITS = "#{FILES_PATH}/meem_inits.csv".freeze
 
 class USER
-	attr_accessor :id, :expiration, :next_due, :check_today, :active
+	attr_accessor :id, :expiration, :next_due, :has_run, :active
 
 	def initialize(user_data)
-		@id = user_data[:borrower_id]
-		@expiration = user_data[:expiration]
-		@next_due = user_data[:next_due]
-		@check_today = user_data[:check_today]
-		@active = user_data[:active]
+		@id = user_data['ACCOUNT']
+		@expiration = user_data['EXPIRATION']
+		@next_due = user_data['NEXT_DUE']
+		@has_run = user_data['HAS_RUN'] == 'true'
+		@active = user_data['ACTIVE'] == 'true'
 	end
 end
 
@@ -51,7 +50,7 @@ class Book
 		if @author.ord == 160
 			@author = EDITOR_REGEX.match(record.text)[1]
 		else
-			@author = (AUTHOR_REGEX.match(@author)).nil? ? '' : $1
+			@author = AUTHOR_REGEX.match(@author).nil? ? '' : $1
 		end
 		key_cond = (rk = record.at(RENEW_KEYS)).nil?
 		@renew_key = rk['value'] unless key_cond
@@ -92,7 +91,8 @@ def expiration_path(page, user)
 		info_page = form.submit
 		# info_page =	Nokogiri::HTML(open("#{FILES_PATH}/expiration.html")) # for testing.
 
-		expires_td = info_page.search(EXPIRATION_COL_SEL).detect{|a|a.text =~/expires/i}
+		expires_col = info_page.search(EXPIRATION_COL_SEL)
+		expires_td = expires_col.detect { |a| a.text =~/expires/i }
 		user.expiration = expires_td.at('.//following-sibling::td').text
 
 		expire_msg_cond = str_to_date(user.expiration) < NOW
@@ -112,12 +112,14 @@ def renewal_path(page, user)
 	next_due = str_to_date(user.next_due)
 
 	if (renew_cond = NOW > next_due - DUE_WINDOW)
-		puts "\n\nRENEW PATH\n\n"
+		print("\n\nRENEW PATH\n\n")
 
 		book_data = get_book_data(page)
 
 		# renew loop: get renew_keys for books due within the 5 days.
-		books_due = book_data.select{|book| str_to_date(book.due) - NOW < DUE_WINDOW}
+		books_due = book_data.select do |book|
+			str_to_date(book.due) - NOW < DUE_WINDOW
+		end
 
 		renew_keys = books_due.each do |book|
 			get_book_data(page)
@@ -130,18 +132,19 @@ def renewal_path(page, user)
 
 		# stores new items_out
 		book_data = get_book_data(page)
-		print_book_data(book_data, user)
 
-		# stores todays date and breaks out if book_data is empty
-		if book_data.empty?
-			user.check_today = Date.today.strftime('%m/%d/%Y')
-		else
+		# updates record
+		if book_data.any?
+			print_book_data(book_data, user)
 			next_due = book_data.min_by{|book| str_to_date(book_data[0].due)}.due
 			user.next_due = next_due
 
 			renew_msg_cond = NOW > str_to_date(user.next_due) - DUE_WINDOW
 			@renew_msg = renew_msg_cond ? 'Renewal Failed!!' : 'Books Renewed'
 		end
+
+		# Notes that program has run for User
+		user.has_run = true
 	end
 end
 
@@ -165,42 +168,31 @@ def process(user)
 	page = form.submit
 end
 
+# TODO: rewrite with correct implementation of headers
 def updateUsers(users)
-  CSV.open(MEEM_INITS, 'w'){|csv| csv << DATA_HEADERS }
-
   users.each do |user|
-		CSV.open(MEEM_INITS, 'a') do |csv|
+		CSV.open(MEEM_INITS, 'w', write_headers: HEADERS) do |csv|
 			csv << [user.id, user.expiration, user.next_due,
-							user.check_today, user.active]
+							user.has_run, user.active]
 		end
 	end
 end
 
-def check_today?(user)
-	str_to_date(user.check_today) < NOW
-end
-
-def should_i_run?(user)
+def should_run?(user)
 	next_expires = str_to_date(user.expiration)
 	next_due = str_to_date(user.next_due)
 
 	next_expires <= NOW + 3 || NOW > next_due - DUE_WINDOW
 end
 
-def read_csv(file_name)# :: CSV -> [{UserData}]
-	csv = CSV.read(MEEM_INITS)
-  csv.drop(1).map do |user_data|
-  	KEYS.zip(user_data).inject({}) do |h, kv|
-  		h.merge({kv[0] => kv[1]})
-  	end
-  end
+def read_csv(file_name)
+	csv = CSV.read(MEEM_INITS, headers: true, return_headers: false)
 end
 
 def meem_library_main # Check each Account and Renew if Necessary.
 	users = read_csv('meem_inits.csv').map { |record| USER.new(record) }
-
 	users.each do |user|
-		if check_today?(user) && should_i_run?(user) && user.active.strip == 'true'
+		if user.active && !user.has_run && should_run?(user)
 			puts "I should run"
 			process(user)
 		end
