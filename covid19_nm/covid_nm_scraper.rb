5dbbea56fc3252facf18ed6dad4bdc2fc85e9076
Jon Zingale <jonzingale@gmail.com>
2020-03-17 20:14:17 -0600

    better parsing

--- covid19_nm/covid_nm_scraper.rb
+++ covid19_nm/covid_nm_scraper.rb
@@ -13,24 +13,29 @@ DATA_CSV = "data.csv".freeze
 CASE_REGEX = /currently (\d+|no) cases/i
 DEATH_REGEX = /(\d+|no) ?reported? deaths/i
 RECOVERY_REGEX = /(\d+|no) ?reported? recoveries/i
-COUNTIES_REGEX = /"data":\[\[(.+)\]\]/
+COUNTY_DATA_REGEX = /\[\"(\w+\W?\w+ ?\w*)\",\"(\d+)\"/
+COUNTIES_REGEX = /"data":\[\[(.+),\[null/
 
 class Agent
   attr_accessor :body, :counties, :total_cases, :deaths, :recoveries
 
-  def initialize
-    @body = get_body
+  def initialize(use_fixture=false)
+    @body = get_body(use_fixture)
     @counties = get_effected_counties
     @total_cases = get_case_by_type(CASE_REGEX)
     @deaths = get_case_by_type(DEATH_REGEX)
     @recoveries = get_case_by_type(RECOVERY_REGEX)
   end
 
-  def get_body
-    agent = Mechanize.new
-    agent.redirect_ok = true
-    landing_page = agent.get(COVID_URL)
-    landing_page.body
+  def get_body(use_fixture)
+    if use_fixture
+      File.read('./body_fixture.html')
+    else
+      agent = Mechanize.new
+      agent.redirect_ok = true
+      landing_page = agent.get(COVID_URL)
+      landing_page.body
+    end
   end
 
   def get_case_by_type(regex)
@@ -43,10 +48,11 @@ class Agent
   end
 
   def get_effected_counties
-    json_match = /"data":\[\[(.+)\]\]/.match @body
-    json = JSON.parse("{#{json_match}}")['data'][0]
-    json.each_with_object({}) do |rec, hh|
-      rec[1] = 0 unless present?(rec[1])
+    json_match = COUNTIES_REGEX.match(@body)[1]
+    clean_match = json_match.gsub('""','"0"')
+    county_array = clean_match.scan(COUNTY_DATA_REGEX)
+
+    county_array.each_with_object({}) do |rec, hh|
       hh.merge!(rec[0] => rec[1].to_i)
     end
   end
@@ -57,11 +63,12 @@ def return_covid19_results
 end
 
 def process
-  agent = Agent.new
+  use_fixture = false
+  agent = Agent.new(use_fixture)
 
   CSV.open(DATA_CSV, 'a') do |csv|
     data = [DATE, TIME, agent.total_cases, agent.deaths, agent.recoveries]
-    data << agent.counties.to_s
+    data << agent.counties.to_json
     csv << data
   end
 end
