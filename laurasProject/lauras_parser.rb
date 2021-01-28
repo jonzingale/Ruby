8b4476b63c65fdcae9378eca4e9dfdcf1b0636b5
Jon Zingale <jonzingale@gmail.com>
2021-01-27 17:05:43 -0700

    lauras parser

--- laurasProject/lauras_parser.rb
+++ laurasProject/lauras_parser.rb
@@ -1,5 +1,28 @@
-require 'byebug'
+require 'csv'
 
-@read = File.open(path,'r').read
+REGEX = /or resident/i
+TEXT_FILE = 'postcards.txt'
+CSV_FILE = 'postcards.csv'
 
-byebug ; 3
+text = File.open(TEXT_FILE,'r').read.gsub("\r", '').split("\n")
+
+idxs = []
+text.each_with_index { |l, i| idxs << i - 1 if REGEX.match(l) }
+
+data = []
+idxs.each do |i|
+  fullname = text[i]
+
+  if /PO Box/i.match(text[i+2])
+    address, street = [text[i+2], nil]
+  else
+    address, street = text[i+2].split(/ /, 2)
+  end
+
+  city, state, zip = text[i+3].split(/ /, 3)
+  data << [fullname, address, street, city, state, zip]
+end
+
+CSV.open(CSV_FILE, 'w') do |csv|
+  data.each { |row| csv << row }
+end
